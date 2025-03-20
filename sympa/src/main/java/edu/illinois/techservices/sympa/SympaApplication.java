401422285cc141e7b6c999f5b9ef1eae0b2370a4
pbale <pbale@illinois.edu>
2025-03-20 12:41:47 -0400

    Initial working springboot application created with Spring Initializer.

---
+++ sympa/src/main/java/edu/illinois/techservices/sympa/SympaApplication.java
@@ -0,0 +1,13 @@
+package edu.illinois.techservices.sympa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SympaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SympaApplication.class, args);
+	}
+
+}
---
+++ sympa/src/test/java/edu/illinois/techservices/sympa/SympaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.illinois.techservices.sympa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SympaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
